aed0d6444351459d1f025207f93eaae0262d695e
kunaia <alekoxanta@gmail.com>
2023-05-09 14:26:48 +0400

    added example test

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+calculator functions
+"""
+
+
+def add(x, y):
+    """Adds two numbers and returns result"""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtracts y from x"""
+    return x - y
